a9a3a2e180ac9b7e1e57ea89cb0367d33e435628
Stefan Prokop <sprokop1010@seznam.cz>
2022-10-21 13:15:56 +0200

    feat: add counter trigger

---
+++ functions/src/index.ts
@@ -0,0 +1,24 @@
+import * as functions from "firebase-functions";
+import axios from "axios";
+
+const REGION = "europe-west3";
+
+export const syncSweepstakePoints = functions
+    .region(REGION)
+    .pubsub
+    .schedule("30 * * * *")
+    .timeZone("Europe/Madrid")
+    .onRun(async () => {
+      console.log(`syncSweepstakePoints is running at ${Date.now()}`);
+      const res = await axios.post(
+          `${process.env.API_BASE_URL}/api/v1/counters`,
+          null,
+          {
+            params: {apiKey: process.env.COUNTER_API_KEY},
+          }
+      );
+      console.log(
+          `syncSweepstakePoints finished at ${Date.now()}`,
+          {response: res.status}
+      );
+    });
