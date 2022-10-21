6694862e5c626758614fbd160b200e2eedd37483
Stefan Prokop <sprokop1010@seznam.cz>
2022-10-21 13:48:02 +0200

    fix: add node-fetch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,27 +1,29 @@
 import * as functions from "firebase-functions";
-import axios from "axios";
+import fetch from "node-fetch";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const REGION = "europe-west3";
 
-export const syncSweepstakePoints = functions
+const runCounterSync = async () => {
+  console.log(`syncSweepstakePoints is running at ${Date.now()}`);
+  const params = new URLSearchParams({
+    apiKey: String(process.env.COUNTER_API_KEY),
+  });
+  const res = await fetch(
+      `${process.env.API_BASE_URL}/api/v1/counters?${params}`,
+      {method: "POST"},
+  );
+  console.log(
+      `syncSweepstakePoints finished at ${Date.now()}`,
+      {response: res.status}
+  );
+};
+
+export const syncCounter = functions
     .region(REGION)
     .pubsub
-    .schedule("30 * * * *")
+    .schedule("55 * * * *")
     .timeZone("Europe/Madrid")
-    .onRun(async () => {
-      console.log(`syncSweepstakePoints is running at ${Date.now()}`);
-      const res = await axios.post(
-          `${process.env.API_BASE_URL}/api/v1/counters`,
-          null,
-          {
-            params: {apiKey: process.env.COUNTER_API_KEY},
-          }
-      );
-      console.log(
-          `syncSweepstakePoints finished at ${Date.now()}`,
-          {response: res.status}
-      );
-    });
+    .onRun(runCounterSync);
